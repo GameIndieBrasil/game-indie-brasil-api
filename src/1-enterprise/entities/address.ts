8b67d20598790161113d1fdd79cdc2062bde1f7f
titoncio <titoncio@gmail.com>
2022-03-28 12:45:32 -0300

    Set dates into numbers

--- src/1-enterprise/entities/address.ts
+++ src/1-enterprise/entities/address.ts
@@ -1,14 +1,8 @@
 // TODO: Avaliar uso de API do IBGE ou importar os dados para cá
 export interface Address {
-<<<<<<< HEAD
-    id: string
-    createdAt: number
-    updatedAt: number
-=======
     id: number
     createdAt: number
     updatedAt: number
->>>>>>> parent of 429dd0d (Set default ids to string (planning to use uuid))
     cep: string
     number: number
     complement: string
--- src/1-enterprise/entities/companyGame.ts
+++ src/1-enterprise/entities/companyGame.ts
@@ -1,16 +1,8 @@
 export interface CompanyGame {
-<<<<<<< HEAD
-    id: string
-    createdAt: number
-    updatedAt: number
-    companyId: string
-    gameId: string
-=======
     id: number
     createdAt: number
     updatedAt: number
     companyId: number
     gameId: number
->>>>>>> parent of 429dd0d (Set default ids to string (planning to use uuid))
     involvement: string
 }
