706eb073ce0aaf8af099891d8530d7ef24a8bfb4
titoncio <titoncio@gmail.com>
2022-03-29 17:48:57 -0300

    Update index to first tests

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-// import { Request, Response, NextFunction } from 'express'
-// import axios, { AxiosResponse } from 'axios'
-// import "reflect-metadata"
+import express from "express";
+import { prizeRouter } from '#framework/functions/handler';
+import { apiVersion } from "#enterprise/consts/config";
 
-console.log('sucesso')
+const app = express();
+
+app.use(`/${apiVersion}/`, prizeRouter)
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+    console.log(`Listener started with settings: ${JSON.stringify(listener.address())}`)
+})
