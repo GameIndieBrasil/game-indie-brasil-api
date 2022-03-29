350a9fcf4576fff6b15a4b00e65de982c05617f1
titoncio <titoncio@gmail.com>
2022-03-29 17:31:35 -0300

    Simplify output dto

--- src/2-application/dto/output.ts
+++ src/2-application/dto/output.ts
@@ -1,17 +1,7 @@
-import { serverName } from "#enterprise/consts/config"
-
 export class Output<T> {
-    statusCode = 500
+    status = 500
     data: T | string = 'Internal server error'
-    headers: {
-        'X-Powered-By'?: string
-        'Access-Control-Allow-Methods'?: string
-    } = {}
     constructor(obj: Partial<Output<T>>) {
-        this.headers = {
-            "X-Powered-By": serverName,
-            "Access-Control-Allow-Methods": 'GET, POST, PUT, PATCH, DELETE'
-        }
         Object.assign(this, obj)
     }
 }
